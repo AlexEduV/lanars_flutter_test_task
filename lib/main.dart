3c5888433162ddd35ead2b1b32d69063f8cac9ac
Alexander Aks'onov <m.mirvua@gmail.com>
2024-12-06 13:30:36 +0200

    reformatting in main;

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lanars_flutter_test_task/presentation/pages/home_page/home_page.dart';
 import 'package:lanars_flutter_test_task/presentation/pages/login_page/login_page.dart';
 
 void main() {
@@ -9,40 +8,40 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const lightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFF0061A6),
+    onPrimary: Color(0xFFFFFFFF),
+    primaryContainer: Color(0xFFD2E4FF),
+    onPrimaryContainer: Color(0xFF001C37),
+    secondary: Color(0xFF535F70),
+    onSecondary: Color(0xFFFFFFFF),
+    secondaryContainer: Color(0xFFD7E3F8),
+    onSecondaryContainer: Color(0xFF101C2B),
+    tertiary: Color(0xFF6B5778),
+    onTertiary: Color(0xFFFFFFFF),
+    tertiaryContainer: Color(0xFFF3DAFF),
+    onTertiaryContainer: Color(0xFF251431),
+    error: Color(0xFFBA1A1A),
+    onError: Color(0xFFFFFFFF),
+    errorContainer: Color(0xFFFFDAD6),
+    onErrorContainer: Color(0xFF410002),
+    outline: Color(0xFF73777F),
+    surface: Color(0xFFFAF9FC),
+    onSurface: Color(0xFF1A1C1E),
+    surfaceContainerHighest: Color(0xFFDFE2EB),
+    onSurfaceVariant: Color(0xFF43474E),
+    inverseSurface: Color(0xFF2F3033),
+    onInverseSurface: Color(0xFFF1F0F4),
+    inversePrimary: Color(0xFFA0CAFF),
+    shadow: Color(0xFF000000),
+    surfaceTint: Color(0xFF0061A6),
+    outlineVariant: Color(0xFFC3C6CF),
+    scrim: Color(0xFF000000),
+  );
+
   @override
   Widget build(BuildContext context) {
-
-    const lightColorScheme = ColorScheme(
-      brightness: Brightness.light,
-      primary: Color(0xFF0061A6),
-      onPrimary: Color(0xFFFFFFFF),
-      primaryContainer: Color(0xFFD2E4FF),
-      onPrimaryContainer: Color(0xFF001C37),
-      secondary: Color(0xFF535F70),
-      onSecondary: Color(0xFFFFFFFF),
-      secondaryContainer: Color(0xFFD7E3F8),
-      onSecondaryContainer: Color(0xFF101C2B),
-      tertiary: Color(0xFF6B5778),
-      onTertiary: Color(0xFFFFFFFF),
-      tertiaryContainer: Color(0xFFF3DAFF),
-      onTertiaryContainer: Color(0xFF251431),
-      error: Color(0xFFBA1A1A),
-      onError: Color(0xFFFFFFFF),
-      errorContainer: Color(0xFFFFDAD6),
-      onErrorContainer: Color(0xFF410002),
-      outline: Color(0xFF73777F),
-      surface: Color(0xFFFAF9FC),
-      onSurface: Color(0xFF1A1C1E),
-      surfaceContainerHighest: Color(0xFFDFE2EB),
-      onSurfaceVariant: Color(0xFF43474E),
-      inverseSurface: Color(0xFF2F3033),
-      onInverseSurface: Color(0xFFF1F0F4),
-      inversePrimary: Color(0xFFA0CAFF),
-      shadow: Color(0xFF000000),
-      surfaceTint: Color(0xFF0061A6),
-      outlineVariant: Color(0xFFC3C6CF),
-      scrim: Color(0xFF000000),
-    );
 
     return MaterialApp(
       title: 'Flutter Demo',
